a3def7a580998732edf7686396643e21e3b7f1f8
Alicja <arisusaktos@gmail.com>
2017-10-30 22:10:07 +0100

    website

---
+++ zakatek/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('.carousel').carousel({
+        interval: false
+    });
+});
+
+
+$(window).scroll(function () {
+
+    scrolled();
+
+});
+
+function scrolled() {
+
+    var windowPosition = $(window).scrollTop();
+
+    var navHeight = $('#main-nav').height();
+
+    if (windowPosition > navHeight) {
+        $('#main-nav').addClass('nav-bg');
+        $('#main-nav').removeClass('nav-bg-remove');
+    } else {
+        $('#main-nav').addClass('nav-bg-remove');
+        $('#main-nav').removeClass('nav-bg');
+    }
+}
