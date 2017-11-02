be95f85fc6e6788106419560c2738a00f2433b37
Alicja <arisusaktos@gmail.com>
2017-11-02 19:12:44 +0100

    updated :baby_chick:

--- zadania-domowe/zadanie-4/js/main.js
+++ zadania-domowe/zadanie-4/js/main.js
@@ -1,14 +1,17 @@
+function createContainer() {
+    var newDiv = $("<div/>")
+        .attr("id", "dane-programisty")
+    $(newDiv).insertAfter("button");
+}
+
 function Pobierz() {
-
     $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
+        var programista = "<p>Imie:" + data.imie + " Nazwisko:" + data.nazwisko + " Zawod:" + data.zawod + " Firma:" + data.firma + "</p>";
 
-        var programista = "Imie:" + data.imie + " Nazwisko:" + data.nazwisko + " Zawod:" + data.zawod + " Firma:" + data.firma;
-
-        var newDiv = $("<div/>")
-            .attr("id", "dane-programisty")
-            .html(programista);
-
-        $(newDiv).insertAfter("button");
+        if ($("#dane-programisty").length === 0) {
+            createContainer();
+        }
+        $('#dane-programisty').append(programista);
 
     });
 
