9134b86d8f411d6a52e7173ded11b8962728460b
Alicja Wolkowska <arisusaktos@gmail.com>
2017-11-09 15:05:23 +0100

    removing old files

--- Games/Pong game/js/messages.js
+++
@@ -1,9 +0,0 @@
-
-
-//
-//var pointsMsg = new Text(
-//  "Points: "+points, 
-//  {fontFamily: "Arial", fontSize: 18, fill: "white"}
-//);
-//pointsMsg.position.set(10, 10);
-//stage.addChild(pointsMsg);
